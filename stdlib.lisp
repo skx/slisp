354d60ef5f448649f160ae08f866ac0a5e0bd19f
Steve Kemp <steve@steve.org.uk>
2026-06-23 10:23:57 +0300

    Added lisp-based standard library to our compiler.
    We embed the contents of `stdlib.lisp` to the user-input, unless
    told not to, and that means we no longer need to copy/paste the
    `print` function in our tests, etc.
    
    This is now documented, and closes #22.

---
+++ stdlib.lisp
@@ -0,0 +1,56 @@
+;;; stdlib.lisp - Lisp standard library, prepended to user programs.
+
+
+;; print the given item intelligently.
+(defun print (x)
+  (if (int? x)
+      (printint x))
+  (if (nil? x)
+      (printstr "<nil>"))
+  (if (str? x)
+      (printstr x))
+  (if (cons? x)
+      (do
+       (putc 40)      ; "("
+       (printcons x)  ; List items, separated by spaces
+        (putc 41)     ; ")"
+      )))
+
+(defun printcons (x)
+  (print (car x))
+  (if (nil? (cdr x))
+      nil
+      (if (cons? (cdr x))
+          (do
+            (putc 32)
+            (printcons (cdr x)))
+          (do
+            (printstr " . ")
+            (print (cdr x))))))
+
+;; exactly like print, but with a newline on the end.
+(defun println (x)
+  (print x)
+  (newline))
+
+
+;; Create a new list by calling the given function for every list element
+(defun map (fn lst)
+  (if (nil? lst)
+      nil
+      (cons
+        (fn (car lst))
+        (map fn (cdr lst)))))
+
+;; count the length of the given list
+(defun length (xs)
+  (if xs
+      (+ 1 (length (cdr xs)))
+      0))
+
+;; sum all the numbers in the list
+(defun sum (xs)
+  (if xs
+      (+ (car xs)
+         (sum (cdr xs)))
+      0))
