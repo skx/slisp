979250744e1f515e93692cf7aab74d20c7ea61be
Steve Kemp <steve@steve.org.uk>
2026-06-24 17:20:16 +0300

    Allow functions to contain documentation.
    This closes #33, by allowing functions to contain a string with
    inline documentation.  This string is discarded, and ignored, but
    it is parsed and skipped if present.
    
    If we had a REPL we could use it to offer help, or similar.  Without
    that it's just a neat feature for users of the standard-library
    routines.

--- test/escapes.lisp
+++ test/escapes.lisp
@@ -1,4 +1,5 @@
 ;;; prove that "\t", "\n", etc, work
 
 (defun main()
+  "This function just tests that '\n', '\t', and similar escapes, work."
   (print "This is a string.\n\t1\t2\n\t3\t4\n\nOK!"))
