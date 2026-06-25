d0484a8442e1e34f58d70f8a09e01be16911dffe
Steve Kemp <steve@steve.org.uk>
2026-06-25 12:29:57 +0300

    Implemented 'strcmp'.

--- test/string.lisp
+++ test/string.lisp
@@ -4,9 +4,25 @@
   (print "' is ")
   (println (strlen x)))
 
+(defun showCmp( a b )
+  (print "comparing a:")
+  (print a)
+  (print " with b:")
+  (print b)
+  (print " result: ")
+  (println (strcmp a b)))
+
+
 (defun main ()
   "Test strlen/strcmp"
 
+  ; strlen test
   (showLen "Steve")
   (showLen "")
-  )
+
+  ; strcmp test
+  (showCmp "Steve" "Steve")
+  (showCmp "Steve" "Rteve")
+
+  ; These should be identical
+  (showCmp "Hello" (implode (explode "Hello"))))
