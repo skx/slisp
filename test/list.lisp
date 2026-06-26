c778e62b6060ab77b01130739370cd91688bd803
Steve Kemp <steve@steve.org.uk>
2026-06-26 14:20:40 +0300

    Implement `min`, `max` and `reduce`.
    `reduce` was used to implement the other two functions, and is nice
    to have.
    
    This closes #57

--- test/list.lisp
+++ test/list.lisp
@@ -1,9 +1,19 @@
 (defun main ()
-  (let ((lst (list 3 4 5 (list 5 2 1) (list 3 12 "Steve"))))
+  (let ((lst (list 3 4 5 (list 5 2 1) (list 3 12 99))))
     (print "Original list: ")
     (print lst)
     (newline)
 
     (print "Flatted list: ")
     (print (flatten lst))
-    (newline)))
+    (newline)
+
+    (print "Minimum entry in list: ")
+    (print (min (flatten lst)))
+    (newline)
+
+    (print "Maximum entry in list: ")
+    (print (max (flatten lst)))
+    (newline)
+
+  ))
