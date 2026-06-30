eda3b39e95ecf5fb56702705626a96acc14b3d20
Steve Kemp <steve@steve.org.uk>
2026-06-30 11:01:02 +0300

    Floating point addition works

--- test/float.lisp
+++ test/float.lisp
@@ -1,4 +1,13 @@
 (defun main()
   "Print floating point numbers!"
-  (println (float1))
-  (println (float2)))
+
+  ; float1 is a function that returns a float
+  (print (float1))
+  (print " x 2 = ")
+  (println (+ (float1) (float1)))
+
+  ; So that was float+float
+  (println "mixed maths:")
+  (println (+ 1 (float1)))
+  (println (+ (float1) 1))
+  )
