d181cf921842b3ddab31d6264e127cd3eb9e76aa
Steve Kemp <steve@steve.org.uk>
2026-07-03 22:21:45 +0300

    Updated now `and` and `or` use variable args

--- test/logical.lisp
+++ test/logical.lisp
@@ -1,20 +1,20 @@
 (defun main ()
-  
+
   (println "start")
 
   ; and; true
-  (if (and (list t t))
+  (if (and t t)
       (println "and is OK"))
   ; and; false
-  (if (and (list t t t t nil t))
+  (if (and t t t t nil t)
      (println "not ok")
      (println "Still okay!"))
 
   ; or: true
-  (if (or (list t nil nil nil ))
+  (if (or t nil nil nil)
       (println "or is OK"))
   ; or: false
-  (if (or (list nil nil nil ))
+  (if (or nil nil nil)
       (println "not OK")
       (println "OR is still okay :)"))
 
