1f2b5697a5fa155350e04143cabbaa3fb3020262
Steve Kemp <steve@steve.org.uk>
2026-07-05 17:21:02 +0300

    Updated comments on eample

--- test/fwrite.lisp
+++ test/fwrite.lisp
@@ -1,14 +1,20 @@
 (defun read (file)
   "Read and return the contents of the named file,
- return NIL on failure to open, or read."
-  (let ((handle (fopen file "r")))
-    (if handle
-        (let ((data (fread handle)))
-          (fclose handle)
-          data))))
+returning NIL on failure.
+
+   The fread, fwrite, and fclose functions each allow
+a NIL file-handle to be specified to allow this simple
+pattern of using a (let ..) scope to handle the I/O."
+  (let ((handle  (fopen file "r"))    ; open
+        (result  (fread handle))      ; read
+        (discard (fclose handle)))    ; close
+    result))
 
 (defun write(file str)
-  "Write the string to the given file"
+  "Write the string to the given file.
+
+Similar story here with regard to the fwrite/close on
+a file-handle that might be nil."
   (let ((handle  (fopen file "w"))                 ; open
         (result  (fwrite handle str (length str))) ; write
         (discard (fclose handle)))                 ; close
