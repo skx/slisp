140e92a4a8ee8e7ef725a600b12e196680ccd8b0
Steve Kemp <steve@steve.org.uk>
2026-07-19 12:53:42 +0300

    Move the lisp reader code into its own package

---
+++ examples/lisp-reader.lisp
@@ -0,0 +1,263 @@
+;;
+;; The lisp-reader package reads source code and returns lisp objects.
+;;
+;; It is designed to be included in other programs, and is included
+;; in that way by inception.lisp.
+;;
+;; There is a standalone `main` function at the foot of this file for
+;; running a self-contained test.  When other programs include it
+;; their own main functions will override the one here.
+;;
+
+;; The source we parse goes here.
+(defvar *reader-text* "")
+
+;; The position we're at goes here.
+(defvar *reader-pos* 0)
+
+;; Helper.
+(defun symbol (name)
+  (list "symbol" name))
+
+
+(defun reader-init (text)
+  (set! *reader-text* text)
+  (set! *reader-pos* 0))
+
+(defun reader-eof ()
+  (>= *reader-pos*
+      (strlen *reader-text*)))
+
+(defun reader-peek ()
+  (if (reader-eof)
+      ""
+      (substr
+       *reader-text*
+       *reader-pos*
+       1)))
+
+(defun reader-next ()
+  (let ((ch (reader-peek)))
+    (set! *reader-pos* (+ *reader-pos* 1))
+    ch))
+
+
+;; Is the given value a number component "-", ".", and "0-9"
+(defun numeric-char? (ch)
+  (or
+   (= ch "-")
+   (= ch ".")
+   (= ch "0")
+   (= ch "1")
+   (= ch "2")
+   (= ch "3")
+   (= ch "4")
+   (= ch "5")
+   (= ch "6")
+   (= ch "7")
+   (= ch "8")
+   (= ch "9")))
+
+;; Is the given string 100% numeric-plausible?
+(defun number-string? (s)
+  (cond
+    ((= s "") nil)
+    ((= (substr s 0 1) "-")
+     (and
+      (> (strlen s) 1)
+      (number-string-aux s 1)))
+    (t
+     (number-string-aux s 0))))
+
+;; helper
+(defun number-string-aux (s pos)
+  (if (>= pos (strlen s))
+      t
+      (if (numeric-char?
+           (substr s pos 1))
+          (number-string-aux
+           s
+           (+ pos 1))
+          nil)))
+
+
+;; skip over whitespace and comments
+(defun reader-skip ()
+  (let ((again t))
+    (while again
+      (set! again nil)
+
+      ;; Skip whitespace
+      (while
+          (or
+           (= (reader-peek) " ")
+           (= (reader-peek) "\t")
+           (= (reader-peek) "\n")
+           (= (reader-peek) "\r"))
+        (reader-next))
+
+      ;; Skip a comment
+      (if (= (reader-peek) ";")
+          (let ()
+            (reader-skip-comment)
+            (set! again t))))))
+
+(defun reader-read ()
+  (reader-skip)
+  (cond
+    ((= (reader-peek) "(")
+     (reader-read-list))
+    ((= (reader-peek) ")")
+     (do
+      (println "unexpected ')' character.  Aborting")
+      (exit 1)))
+    ((= (reader-peek) "\"")
+     (reader-read-string))
+    ((= (reader-peek) "'")
+     (reader-next)
+     (list
+      (symbol "quote")
+      (reader-read)))
+    (t
+     (reader-read-atom))))
+
+(defun reader-skip-comment ()
+  ;; consume the ';'
+  (reader-next)
+
+  ;; skip until newline or EOF
+  (while
+      (and
+       (!= (reader-peek) "")
+       (!= (reader-peek) "\n"))
+    (reader-next))
+  ;; consume the newline if present
+  (if (= (reader-peek) "\n")
+      (reader-next)))
+
+(defun reader-read-list ()
+  ;; consume '('
+  (reader-next)
+  (let ((items nil))
+    (reader-skip)
+    (while (!= (reader-peek) ")")
+      (set! items
+            (cons
+             (reader-read)
+             items))
+      (reader-skip))
+    ;; consume ')'
+    (reader-next)
+    (reverse items)))
+
+(defun reader-read-string ()
+  (reader-next)   ; consume opening "
+
+  (let ((text ""))
+
+    (while
+        (and
+         (!= (reader-peek) "")
+         (!= (reader-peek) "\""))
+
+      (if (= (reader-peek) "\\")
+          (do
+           (reader-next)     ; consume '\'
+
+           (let ((ch (reader-next)))
+             (set! text
+                   (strcat
+                    text
+                    (cond
+                      ((= ch "n") "\n")
+                      ((= ch "t") "\t")
+                      ((= ch "\"") "\"")
+                      ((= ch "\\") "\\")
+                      (t ch))))))
+          (set! text
+                (strcat
+                 text
+                 (reader-next)))))
+
+    (reader-next)    ; closing "
+    text))
+
+(defun reader-read-string-old ()
+  ;; consume opening "
+  (reader-next)
+  (let ((text ""))
+    (while
+        (and
+         (!= (reader-peek) "")
+         (!= (reader-peek) "\""))
+      (set! text
+            (strcat
+             text
+             (reader-next))))
+
+    ;; consume closing "
+    (if (= (reader-peek) "\"")
+        (reader-next))
+    text))
+
+(defun reader-read-atom ()
+  (let ((token ""))
+    (while
+        (and
+         (!= (reader-peek) "")
+         (!= (reader-peek) " ")
+         (!= (reader-peek) "\t")
+         (!= (reader-peek) "\r")
+         (!= (reader-peek) "\n")
+         (!= (reader-peek) "(")
+         (!= (reader-peek) ")"))
+
+      (set! token
+            (strcat
+             token
+             (reader-next))))
+
+    ;; Is this a number?
+    (if (number-string? token)
+        (atof token)
+        (symbol token))))
+
+
+;; parse a complete program and return all the expressions within it.
+(defun reader-parse-program ()
+  (let ((forms nil))
+    (reader-skip)
+    (while (not (reader-eof))
+      (set! forms
+            (cons
+             (reader-read)
+             forms))
+      (reader-skip))
+    (reverse forms)
+    ))
+
+
+;;;
+;;; Test Code
+;;;
+(defun reader-dump-program(path)
+  (let ((handle (fopen path "r"))  ; open
+        (data   (fread handle))    ; read
+        (res    (fclose handle)))  ; close
+    (when data
+      (reader-init data)
+      (println path ":")
+      (println (reader-parse-program)))))
+
+
+;;
+;; Dump all arguments, or just the named one.
+;;
+;; args.lisp is small enough to be parsed quickly.
+;;
+(defun main(args)
+  (if (nil? args)
+      (reader-dump-program "args.lisp")
+      (if (= 1 (length args))
+          (reader-dump-program "args.lisp")
+          (map (lambda (x) (reader-dump-program x)) (cdr args)))))
