4b5e1796261a99ae67a8bb0225ae490488a6da62
Steve Kemp <steve@steve.org.uk>
2026-07-29 20:37:14 +0300

    Updated test to segfault on main, but work on this branch

--- examples/globals.lisp
+++ examples/globals.lisp
@@ -6,6 +6,32 @@
 ;; Attempting to change this will generate an error at compile-time.
 (defconst bar "i am unchanging")
 
+;; Declare some colours, as RGB values.
+(defvar blue    (list   0   0 255))
+(defvar midblue (list 20 174 244))
+(defvar pink    (list 255 192 203))
+(defvar green   (list   0 255   0))
+(defvar yellow  (list 255 255 224))
+(defvar cyan    (list   0 244 244))
+(defvar mustard (list 255 206  27))
+
+;; A scene we might pretend to draw.
+;;
+;; Mostly here to confirm we load things correctly in our
+;; init-code
+;;
+;; Ordering is important as we have no Z-index.
+(defvar scene
+  (list
+   (list "box" 350 150 400 300 pink)
+   (list "circle" 20 20 15 mustard)
+   (list "box" 0 0 20 400 blue)
+   (list "circle" 20 20 75 midblue)
+   (list "circle" 20 20 175  cyan)
+   (list "circle" 320 210 75 green )
+   (list "background" yellow)))
+
+
 (defun local ()
   "Test scoping by having a local variable with the same name as a global.
 
@@ -21,6 +47,8 @@ Spoiler: Local variable always comes first."
 (defun main (args)
   "Entry Point."
 
+  (sys-gc)
+
   ;; show global foo
   (println "global variable is:" foo)
 
@@ -32,6 +60,8 @@ Spoiler: Local variable always comes first."
 
   ;; Call a local function
   (local)
+
+  (sys-gc)
 
   (println "Global variable untouched:" foo)
 
